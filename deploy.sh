59d2d17b5a3b7a3c67450a70b0c4e145ee9a9fbb
qyd <saviorbruce@gmail.com>
2018-02-07 08:39:05 -0800

    fix bug from circleci tutorial, should use PROJECT_ID instead of PROJECT_NAME

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/sirius
+sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_ID}/sirius
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment cluster-sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/'"$PROJECT_NAME"'/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment cluster-sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/'"$PROJECT_ID"'/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
 
