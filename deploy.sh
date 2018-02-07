cde4565789f4f800ef85391fb88310c68b00b55d
qyd <saviorbruce@gmail.com>
2018-02-07 11:48:18 -0800

    try kubectl deployments

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_ID}/sirius
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment cluster-sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/'"$PROJECT_ID"'/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/'"$PROJECT_ID"'/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
 
