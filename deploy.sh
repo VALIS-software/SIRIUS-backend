6fb7ae37144defa375add3e7250dae55172d2bcc
qyd <saviorbruce@gmail.com>
2018-02-07 01:08:13 -0800

    add curl command in test and make deploy.sh executable

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/sirius
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/VALIS/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment cluster-sirius -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius","image":"us.gcr.io/'"$PROJECT_NAME"'/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
 
