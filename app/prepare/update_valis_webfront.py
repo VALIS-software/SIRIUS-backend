683b8021782655fe47324dc1dbcac9ebbf5c17ad
qyd <saviorbruce@gmail.com>
2018-02-11 15:02:18 -0800

    add new script in app/prepare folder to update the valis webfront. This tool will be called by run.py, and can be called separately by itself. This is going to help our hot-patch when only valis is changed

---
+++ app/prepare/update_valis_webfront.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os, shutil
+from google.cloud import storage
+from zipfile import ZipFile
+
+def update_valis_webfront():
+    # Update /static/dist with the latest dist.zip file on our Google Cloud
+    # We assume that the environment variable GOOGLE_APPLICATION_CREDENTIALS is set, or we are running in a cloud compute engine or kubernete engine
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('valis-front-dev')
+    blob = bucket.get_blob('dist/dist.zip')
+    print("Connected to google cloud. File dist/dist.zip found.")
+    # download the archieve to tmp_path
+    tmp_path = "tmp_dist"
+    if os.path.isdir(tmp_path):
+        shutil.rmtree(tmp_path)
+    os.mkdir(tmp_path)
+    dest_fname = os.path.join(tmp_path, 'dist.zip')
+    blob.download_to_filename(dest_fname)
+    print("File download finished.")
+    
+    # unzip
+    with ZipFile(dest_fname) as zf:
+        zf.extractall(tmp_path)
+    # We will leave the zip file for debugging
+    # os.remove(dest_fname)
+    print("Unzip finished.")
+    
+    # target folder to put our front-end js code
+    # We use the relative path to the location of this file, to allow running this script from anywhere
+    this_file_folder = os.path.dirname(os.path.realpath(__file__))
+    target_path = os.path.join(this_file_folder, "../static/dist")
+    # check if the target folder already exists, if so, remove it
+    if os.path.isdir(target_path):
+        shutil.rmtree(target_path)
+    shutil.move(tmp_path, target_path)
+    print("static/dist folder was replaced. All step finished!")
+
+if __name__ == "__main__":
+     update_valis_webfront()
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from app import app
+from app.prepare.update_valis_webfront import update_valis_webfront
+
+update_valis_webfront()
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000)
