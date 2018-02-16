8d88ca9aa20a5db0cf462506b42b4d5b2ef648ee
qyd <saviorbruce@gmail.com>
2018-02-15 23:50:37 -0800

    use connect=False to walk around the connection issue of mongodb caused by uwsgi

--- app/sirius/mongo/__init__.py
+++ app/sirius/mongo/__init__.py
@@ -2,9 +2,9 @@ import os
 from pymongo import MongoClient
 
 if 'MONGO_SERVICE_HOST' in os.environ:
-    client = MongoClient('mongodb://mongo:27017')
+    client = MongoClient('mongodb://mongo:27017', connect=False)
 else:
-    client = MongoClient('35.197.96.144', 27017)
+    client = MongoClient('35.197.96.144', 27017, connect=False)
 
 db = client.database
 GenomeNodes = db.GenomeNodes
