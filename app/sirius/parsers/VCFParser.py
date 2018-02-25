cf532d71ff9e22c3e80d2514f041110f95315cdb
qyd <saviorbruce@gmail.com>
2018-02-25 14:56:30 -0800

    update VCFParser for ClinVar, data not uploaded yet

--- app/sirius/parsers/VCFParser.py
+++ app/sirius/parsers/VCFParser.py
@@ -89,54 +89,59 @@ class VCFParser(Parser):
         d['INFO'] = dinfo
         return d
 
+
+class ClinVarVCFParser(VCFParser):
     def get_mongo_nodes(self):
         """ Parse study data into three types of nodes:
         GenomeNode: Information about SNP, unique ID is defined based on rs number
-            Example: { '_id': 'snp_rs143888043',
-                       'type': 'SNP',
-                       'location': 'Chr1',
-                       'start': 1014042,
-                       'end': 1014042,
-                       'length': 1,
-                       'sourceurl': 'clinvar',
-                       'assembly': 'GRCh38',
-                       'info': {'ID': 'rs143888043',
-                                'Name': 'rs143888043',
-                                'v_ref': 'G',
-                                'v_alt': 'A',
-                                'ALLELEID': 446939,
-                                "CLNVCSO": "SO:0001483",
-                                "GENEINFO": "ISG15:9636",
-                                "MC": "SO:0001583|missense_variant",
-                                "ORIGIN": "1",
-                                "CLNHGVS": "NC_000001.11:g.1014042G>A",
-                               }
-                     }
+            Example:     {
+                           "_id": "snp_rs143888043",
+                           "assembly": "GRCh38",
+                           "location": "Chr1",
+                           "sourceurl": "test.vcf",
+                           "type": "SNP",
+                           "start": 1014042,
+                           "end": 1014042,
+                           "length": 1,
+                           "info": {
+                             "variant_ref": "G",
+                             "variant_alt": "A",
+                             "ALLELEID": 446939,
+                             "CLNVCSO": "SO:0001483",
+                             "GENEINFO": "ISG15:9636",
+                             "MC": "SO:0001583|missense_variant",
+                             "ORIGIN": "1",
+                             "CLNHGVS": "NC_000001.11:g.1014042G>A"
+                           }
+                         },
+
         InfoNode: Information about trait, unique ID is defined based on trait name
-            Example: { '_id': "trait_immunodeficiency_38_with_basal_ganglia_calcification",
-                       'type': 'trait',
-                       'name': 'immunodeficiency_38_with_basal_ganglia_calcification',
-                       'sourceurl': sourceurl,
-                       'info': {
-                            'MedGen': 'C4015293',
-                            'OMIM': '616126',
-                            'Orphanet': 'ORPHA319563',
-                            "CLNDN": "Immunodeficiency_38_with_basal_ganglia_calcification",
+            Example:   {
+                         "_id": "trait_Immunodeficiency_38_with_basal_ganglia_calcification",
+                         "type": "trait",
+                         "name": "Immunodeficiency 38 with basal ganglia calcification",
+                         "sourceurl": "test.vcf",
+                         "info": {
+                           "MedGen": "C4015293",
+                           "OMIM": "616126",
+                           "Orphanet": "ORPHA319563"
+                             }
+                        },
 
-
-
-                       }
-                    }
         EdgeNode: Study that connects SNP and trait
-            Example: { 'from_id': 'snp_rs143888043', to_id: 'trait_immunodeficiency_38_with_basal_ganglia_calcification',
-                       'from_type': 'SNP', 'to_type': 'trait',
-                       'type': 'association',
-                       'sourceurl': 'Clinvar',
-                       'info': { "CLNREVSTAT": "criteria_provided,_single_submitter",
-                                 "QUAL": ".",
-                                 "FILTER": "."
-                               }
-                     }
+            Example:     {
+                            "from_id": "snp_rs143888043",
+                            "to_id": "trait_Immunodeficiency_38_with_basal_ganglia_calcification",
+                            "from_type": "SNP",
+                            "to_type": "trait",
+                            "type": "association",
+                            "sourceurl": "test.vcf",
+                            "info": {
+                              "CLNREVSTAT": "criteria_provided,_single_submitter",
+                              "QUAL": ".",
+                              "FILTER": "."
+                            }
+                        },
         """
         genome_nodes, info_nodes, edge_nodes = [], [], []
         known_vid, known_traits = set(), set()
@@ -150,25 +155,6 @@ class VCFParser(Parser):
             sourceurl = self.filename
         seqid_loc = dict()
         for d in self.varients:
-            if 'CLNDN' not in d['INFO']: continue
-            # create InfoNode for trait
-            trait_name = d['INFO']['CLNDN'].split('|',1)[0].lower()
-            trait_id = 'trait_' + trait_name
-            if trait_id not in known_traits:
-                infonode = { '_id': trait_id,
-                             'type': 'trait',
-                             'name': trait_name,
-                             'sourceurl': sourceurl,
-                             'info': dict()
-                           }
-                try:
-                    for nameidx in d['INFO']['CLNDISDB'].split('|',1)[0].split(','):
-                        name, idx = nameidx.split(':', 1)
-                        infonode['info'][name] = idx
-                except:
-                    pass
-                info_nodes.append(infonode)
-                known_traits.add(trait_id)
             # create GenomeNode for Varient
             if 'RS' in d['INFO']:
                 varient_id = "snp_rs" + str(d["INFO"]["RS"])
@@ -183,32 +169,57 @@ class VCFParser(Parser):
                 known_vid.add(varient_id)
                 location = 'Chr' + d['CHROM']
                 gnode = {'_id': varient_id, 'assembly': assembly, 'location':location, 'sourceurl': sourceurl}
-                gnode['Type'] = varient_type
+                gnode['type'] = varient_type
                 gnode['start'] = gnode['end'] = int(d['POS'])
                 gnode['length'] = 1
-                gnode['info'] = {"varient_ref": d["REF"], 'varient_alt': d['ALT']}
+                gnode['info'] = {"variant_ref": d["REF"], 'variant_alt': d['ALT']}
                 for key in ('ALLELEID', 'CLNVCSO', 'GENEINFO', 'MC', 'ORIGIN', 'CLNHGVS'):
                     try:
                         gnode['info'][key] = d["INFO"][key]
                     except:
                         pass
                 genome_nodes.append(gnode)
-
-            # create EdgeNode
-            # add study to edge_nodes
-            edgenode = {'from_id': varient_id , 'to_id': trait_id,
-                        'from_type': varient_type, 'to_type': 'trait',
-                        'type': 'association',
-                        'sourceurl': sourceurl,
-                        'info': {
-                            "CLNREVSTAT": d['INFO']["CLNREVSTAT"],
-                            "QUAL": d['QUAL'],
-                            "FILTER": d['FILTER']
-                        }
-                       }
-            edge_nodes.append(edgenode)
-            if self.verbose and len(edge_nodes) % 100000 == 0:
-                print("%d varients parsed" % len(edge_nodes), end='\r')
+            # we will abandon this entry if no trait information found
+            if 'CLNDN' not in d['INFO'] or 'CLNDISDB' not in d['INFO']: continue
+            # create InfoNode for trait, one entry could have multiple traits
+            this_trait_ids = []
+            trait_names = d['INFO']['CLNDN'].split('|')
+            trait_CLNDISDBs = d['INFO']['CLNDISDB'].split('|')
+            if len(trait_names) != len(trait_CLNDISDBs):
+                print(d)
+                print("Number of traits in in CLNDN and CLNDISDB not consistent! Skipping.")
+                continue
+            for trait_name, trait_disdb in zip(trait_names, trait_CLNDISDBs):
+                trait_id = 'trait_' + trait_name
+                this_trait_ids.append(trait_id)
+                if trait_id not in known_traits:
+                    infonode = { '_id': trait_id,
+                                'type': 'trait',
+                                'name': trait_name.replace("_"," "), # use space here for text search in MongoDB
+                                'sourceurl': sourceurl,
+                                'info': dict()
+                                }
+                    for nameidx in trait_disdb.split(','):
+                        name, idx = nameidx.split(':', 1)
+                        infonode['info'][name] = idx
+                    info_nodes.append(infonode)
+                    known_traits.add(trait_id)
+            # create EdgeNode for each trait in this entry
+            for trait_id in this_trait_ids:
+                # add study to edge_nodes
+                edgenode = {'from_id': varient_id , 'to_id': trait_id,
+                            'from_type': varient_type, 'to_type': 'trait',
+                            'type': 'association',
+                            'sourceurl': sourceurl,
+                            'info': {
+                                "CLNREVSTAT": d['INFO']["CLNREVSTAT"],
+                                "QUAL": d['QUAL'],
+                                "FILTER": d['FILTER']
+                            }
+                           }
+                edge_nodes.append(edgenode)
+                if self.verbose and len(edge_nodes) % 100000 == 0:
+                    print("%d varients parsed" % len(edge_nodes), end='\r')
         return genome_nodes, info_nodes, edge_nodes
 
 
