84975bedb60d4af4dd6da553cb27980105a68658
qyd <saviorbruce@gmail.com>
2018-03-05 16:58:42 -0800

    update mongo server setting to check the linked mongo container first before using localhost

--- app/sirius/mongo/__init__.py
+++ app/sirius/mongo/__init__.py
@@ -12,6 +12,9 @@ except:
 if 'KUBERNETES_SERVICE_HOST' in os.environ:
     # we currently have one replica, more can be added here
     server_address = 'mongo-0.mongo-service'
+elif 'MONGO_PORT' in os.environ:
+    # This is used when we have container linkage like --link some-mongo:mongo when debugging inside docker containers
+    server_address = 'mongo'
 else:
     # use kubectl port-forward mongo-0 27017:27017 to forward port if dev locally
     server_address = 'localhost'
