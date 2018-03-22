f927f29c954a7fe6a03f78ad5e0824e5760a610b
qyd <saviorbruce@gmail.com>
2018-03-22 10:37:31 -0700

    update circleCI config to only deploy for master branch

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,8 +4,7 @@
 set -e
 
 # only push when the git branch is master
-# if [ "$ {CIRCLE_BRANCH}" == "master" ]; then
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
   gcloud docker -- push us.gcr.io/valis-194104/sirius
-  # sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
   kubectl patch deployment sirius-dev -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius-dev","image":"us.gcr.io/valis-194104/sirius:'"$CIRCLE_SHA1"'"}]}}}}'
-# fi
+fi
