5e18f68514fb3c4eed8cb3f0730bb4a1bfb31f0d
qyd <saviorbruce@gmail.com>
2018-03-23 10:28:14 -0700

    small change in build_and_deploy.sh for production

--- config/kubernetes/production/build_and_deploy.sh
+++ config/kubernetes/production/build_and_deploy.sh
@@ -2,7 +2,8 @@
 
 set -e
 SHA=$(echo $(date) | sha1sum | awk '{ print $1 }')
-sudo docker build -t us.gcr.io/valis-194104/sirius:$SHA .
-sudo docker tag us.gcr.io/valis-194104/sirius:$SHA us.gcr.io/valis-194104/sirius:prod
-sudo gcloud docker -- push us.gcr.io/valis-194104/sirius:$SHA
+docker build -t us.gcr.io/valis-194104/sirius:$SHA .
+docker tag us.gcr.io/valis-194104/sirius:$SHA us.gcr.io/valis-194104/sirius:prod
+gcloud docker -- push us.gcr.io/valis-194104/sirius:$SHA
+gcloud docker -- push us.gcr.io/valis-194104/sirius:prod
 kubectl patch deployment sirius-prod -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius-prod","image":"us.gcr.io/valis-194104/sirius:'"$SHA"'"}]}}}}'
