6e404d462635b0e7f1df2b12cce6313133e20feb
qyd <saviorbruce@gmail.com>
2018-03-23 10:09:19 -0700

    improve kubernetes production config scripts

--- config/kubernetes/production/build_and_deploy.sh
+++ config/kubernetes/production/build_and_deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -e
-
-sudo docker build -t us.gcr.io/valis-194104/sirius:prod
-sudo gcloud docker -- push us.gcr.io/valis-194104/sirius:prod
-# use an annotation of date to triggle an roll-out
-d=$(echo $(date) | tr -d ' ') # remove space
-kubectl patch deployment sirius-prod -p '{"spec":{"template":{"metadata":{"annotations":{"date":"'$d'"}}}}}'
+SHA=$(echo $(date) | sha1sum | awk '{ print $1 }')
+sudo docker build -t us.gcr.io/valis-194104/sirius:$SHA .
+sudo docker tag us.gcr.io/valis-194104/sirius:$SHA us.gcr.io/valis-194104/sirius:prod
+sudo gcloud docker -- push us.gcr.io/valis-194104/sirius:$SHA
+kubectl patch deployment sirius-prod -p '{"spec":{"template":{"spec":{"containers":[{"name":"sirius-prod","image":"us.gcr.io/valis-194104/sirius:'"$SHA"'"}]}}}}'
---
+++ config/kubernetes/production/expose.sh
@@ -0,0 +1,1 @@
+kubectl expose deployment sirius-prod --type=LoadBalancer --port 80 --target-port 5000
