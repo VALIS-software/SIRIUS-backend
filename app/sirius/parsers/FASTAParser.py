7108660112305aa25ae65209d3f40263aeecf281
Salik Syed <ssyed@cs.stanford.edu>
2018-04-23 11:57:40 -0700

    generate g-bands (downsample)

--- app/sirius/parsers/FASTAParser.py
+++ app/sirius/parsers/FASTAParser.py
@@ -8,13 +8,39 @@ import gzip
 import os
 import numpy as np
 import tiledb
+import collections
 
-# the exponent for each resolution step
-DOWNSAMPLE_EXPONENT = 16
+DOWNSAMPLE_RESOLUTIONS = [32, 128, 256, 1024, 16384, 65536, 131072]
 
 class FASTAParser(Parser):
+    def generate_downsampled_g_bands(tileServerId, step):
+        """ Downsample the data by counting the percentage of GC content within a [step] sized interval
+            See: https://en.wikipedia.org/wiki/G_banding
+        """
+        ctx = tiledb.Ctx()
+        db = tiledb.DenseArray.load(ctx, tileServerId)
+        sz = len(db)
+        cnt = math.ceil(sz/float(step))
+        newctx = tiledb.Ctx()
+        d1 = tiledb.Dim(ctx, "locus", domain=(0, cnt - 1), tile=math.ceil(cnt/1000.0), dtype="uint64")
+        domain = tiledb.Domain(ctx, d1)
+        gcContent = tiledb.Attr(ctx, "gc", compressor=('lz4', -1), dtype='float32')
+        tileDB_arr = tiledb.DenseArray(ctx, tileServerId + "_" + str(step),
+                  domain=domain,
+                  attrs=[gcContent],
+                  cell_order='row-major',
+                  tile_order='row-major')
+        gcData = []
+        for i in range(0, cnt):
+            counts = collections.Counter(np.array(db[i * step : (i + 1) * step]['value']))
+            # store the GC content
+            percentage = float((counts[b'G'] + counts[b'C'] + counts[b'c'] + counts[b'g'])) / float(step)
+            gcData.append(percentage)
+        tileDB_arr[:] = np.array(gcData, dtype='float32')
 
-    def load_to_tile_db(self, seq_record, tileServerId, resolution_step, min_resolution):
+    def load_to_tile_db(self, seq_record, tileServerId):
+        """ Loads the sequence data into TileDB, generates downsampled tiles 
+        """
         if not os.path.exists(TILE_DB_PATH):
             os.makedirs(TILE_DB_PATH)
         os.chdir(TILE_DB_PATH)
@@ -29,10 +55,12 @@ class FASTAParser(Parser):
                   tile_order='row-major')
 
         tileDB_arr[:] = np.array(seq_record.seq, 'S1')
-        return [1]
+        for resolution in DOWNSAMPLE_RESOLUTIONS:
+            self.generate_downsampled_g_bands(tileServerId, resolution)
+        return DOWNSAMPLE_RESOLUTIONS
 
     def parse(self):
-        """ Parse the FASTA format using BioPython"""
+        """ Parse the raw sequence using BioPython, store data to tileDB and generate InfoNodes"""
         chrIdx = 0
         fname = self.filename
         info_node = {
@@ -57,7 +85,7 @@ class FASTAParser(Parser):
                 else:
                     chrName = "chr" + str(chrIdx + 1)
                 tileServerId = fname + "_" + str(chrIdx)
-                resolutions = self.load_to_tile_db(seq_record, tileServerId, DOWNSAMPLE_EXPONENT, 1024)
+                resolutions = self.load_to_tile_db(seq_record, tileServerId)
                 chrInfo = {
                     "length" : len(seq_record),
                     "tileServerId": tileServerId,
