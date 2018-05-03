d44a5639dca0adda31eeea0b3bb1d6749ba0501d
Salik Syed <ssyed@cs.stanford.edu>
2018-05-03 14:06:23 -0700

    more fixes to bigwig downsampling

--- app/sirius/parsers/BigWigParser.py
+++ app/sirius/parsers/BigWigParser.py
@@ -22,6 +22,7 @@ class BigWigParser(Parser):
         ctx = tiledb.Ctx()
         sz = bigwigRecord.chroms(chromosomeName)
         tile_size = 1000000
+
         d1 = tiledb.Dim(ctx, "locus", domain=(0, sz - 1), tile=tile_size, dtype="uint64")
         domain = tiledb.Domain(ctx, d1)
         signalValue = tiledb.Attr(ctx, "value", compressor=('lz4', -1), dtype='float32')
@@ -32,7 +33,7 @@ class BigWigParser(Parser):
                   tile_order='row-major')
 
         tiles = int(math.ceil(float(sz) / tile_size))
-        print("Starting writing %d tiles to TileDB" % len(tiles)) 
+        print("Writing %d tiles to TileDB" % tiles)
         # write basic signal data
         for i in range(0, tiles):
             start = i * tile_size
@@ -40,47 +41,54 @@ class BigWigParser(Parser):
             values =  bigwigRecord.values(chromosomeName, start, end)
             mainBigWigDB[start:end] = np.array(values, 'float32')
         print("Finished writing data to TileDB")
-        print("Generating summary statistic TileDB table (avg, min, max)")
+        
+
         # create downsampled arrays:
-        for stride in TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS:
-            stride_length = math.ceil(sz/float(stride))
-            d1 = tiledb.Dim(ctx, "locus", domain=(0, stride_length), tile=math.ceil(stride_length/1000.0), dtype="uint64")
-            domain = tiledb.Domain(ctx, d1)
-            vmin = tiledb.Attr(ctx, "min", compressor=('lz4', -1), dtype='float32')
-            vmax = tiledb.Attr(ctx, "max", compressor=('lz4', -1), dtype='float32')
-            vavg = tiledb.Attr(ctx, "avg", compressor=('lz4', -1), dtype='float32')
-            # create a dense array for this stride resolution
-            tiledb.DenseArray(ctx, tileServerId + "_" + str(stride),
-                      domain=domain,
-                      attrs=[vmin, vmax, vavg],
-                      cell_order='row-major',
-                      tile_order='row-major')
-       
         stride_lengths = map(lambda x : math.ceil(sz/float(x)), TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS)
-        stride_data = {}
+        stride_data_avgs = {}
+        stride_data_mins = {}
+        stride_data_maxes = {}
         stride_sums = {}
         stride_mins = {}
         stride_maxes = {}
         
         for stride in TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS:
-            stride_data[stride] = []
+            stride_data_avgs[stride] = []
+            stride_data_mins[stride] = []
+            stride_data_maxes[stride] = []
             stride_sums[stride] = 0
             stride_mins[stride] = None
             stride_maxes[stride] = None
-        print("Computing summary statistics")
+        print("Computing summary statistics (avg, min, max)")
+
+        # load 1mbp tiles into memory for aggregation
+        curr_tile = 0
+        curr_tile_buffer = mainBigWigDB[0:tile_size]["value"]
+
+        # compute aggregations by iterating over each index
         for i in range(0, sz - 2):
-            # read teh value back from tileDB
-            value = mainBigWigDB[i : i+1]["value"][0]
+            if math.floor(i / tile_size) > curr_tile:
+                print("loading tile %d" % (curr_tile + 1))
+                curr_tile = int(i / tile_size)
+                endClamped = int(min((curr_tile + 1)*tile_size, sz - 1))
+                curr_tile_buffer = mainBigWigDB[int(curr_tile*tile_size) : endClamped]["value"]
+            # read the value back from tileDB
+            value = curr_tile_buffer[i - (curr_tile * tile_size)]
+            if math.isnan(value):
+                continue
             for stride in TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS:
                 if (i + 1) % stride == 0:
                     vavg = float(stride_sums[stride]) / stride
                     vmin = stride_mins[stride]
                     vmax = stride_maxes[stride]
-                    stride_data[stride].append([vmin, vmax, vavg])
+                    stride_data_avgs[stride].append(vavg)
+                    stride_data_mins[stride].append(vmin)
+                    stride_data_maxes[stride].append(vmax)
                     stride_sums[stride] = 0
                     stride_mins[stride] = None
                     stride_maxes[stride] = None
                 stride_sums[stride] += value
+
                 if not stride_mins[stride]:
                     stride_mins[stride] = value
 
@@ -88,13 +96,26 @@ class BigWigParser(Parser):
                     stride_maxes[stride] = value
                 stride_mins[stride] = min(stride_mins[stride], value)
                 stride_maxes[stride] = max(stride_maxes[stride], value)
-
         print("Writing summary statistics to TileDB")
         os.chdir(TILE_DB_PATH)
         for stride in TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS:
-            ctx = tiledb.Ctx()
-            downsampled_arr = tiledb.DenseArray.load(ctx, tileServerId + "_" + str(stride))
-            downsampled_arr[:] = np.array(stride_data[stride], dtype='float32')
+            numBins = numBins = len(stride_data_mins[stride])
+            d1 = tiledb.Dim(ctx, "locus", domain=(0, numBins - 1), tile=math.ceil(numBins/1000.0), dtype="uint64")
+            domain = tiledb.Domain(ctx, d1)
+            vmin = tiledb.Attr(ctx, "min", compressor=('lz4', -1), dtype='float32')
+            vmax = tiledb.Attr(ctx, "max", compressor=('lz4', -1), dtype='float32')
+            vavg = tiledb.Attr(ctx, "avg", compressor=('lz4', -1), dtype='float32')
+            # create a dense array for this stride resolution
+            downsampled_arr = tiledb.DenseArray(ctx, tileServerId + "_" + str(stride),
+                      domain=domain,
+                      attrs=[vmin, vmax, vavg],
+                      cell_order='row-major',
+                      tile_order='row-major')
+            downsampled_arr[:] = {
+                'min': np.array(stride_data_mins[stride], dtype='float32'),
+                'max': np.array(stride_data_maxes[stride], dtype='float32'),
+                'avg': np.array(stride_data_avgs[stride], dtype='float32')
+            }
             print("Wrote downsampled array: % d" % stride)
         return TILE_DB_BIGWIG_DOWNSAMPLE_RESOLUTIONS
 
