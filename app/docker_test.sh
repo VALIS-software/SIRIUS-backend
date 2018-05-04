11f82099c9a64d5e0c1a9b955366aab874af9925
qyd <saviorbruce@gmail.com>
2018-05-03 23:24:56 -0700

    reconfig circle CI to connect kubernetes MongoDB inside remote docker

--- app/docker_test.sh
+++ app/docker_test.sh
@@ -2,7 +2,13 @@
 
 echo "Running SIRIUS tests inside Docker container"
 
+/app/prep_gcloud.sh
+
+kubectl port-forward mongo-0 27017:27017 &
+
 cd /app/sirius/tests
 
 python -m unittest discover -q
+
+pkill kubectl
 
