2c4b5748ae5482e1b1f6912f2957156996bb80d8
qyd <saviorbruce@gmail.com>
2018-05-11 12:04:59 -0700

    fix type-o in constants

--- app/sirius/helpers/constants.py
+++ app/sirius/helpers/constants.py
@@ -14,7 +14,7 @@ DATA_SOURCE_ENCODE = 'ENCODE'
 DATA_SOURCE_FASTA = 'RefSeq'
 
 TRACK_TYPE_SEQUENCE = 'track_type_sequence'
-TRACK_TYPE_FUNCTIONAL = 'track_type_funcional'
+TRACK_TYPE_FUNCTIONAL = 'track_type_functional'
 TRACK_TYPE_GENOME = 'track_type_genome'
 TRACK_TYPE_GWAS = 'track_type_gwas'
 TRACK_TYPE_EQTL = 'track_type_eqtl'
