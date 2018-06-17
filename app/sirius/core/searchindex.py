45dabaa53b1bda09dcf50d1705fea0b32c11a9bb
Salik Syed <saliksyed@Saliks-MBP.fios-router.home>
2018-06-16 17:59:26 -0700

    getting closer to quality results

--- app/sirius/core/searchindex.py
+++ app/sirius/core/searchindex.py
@@ -1,8 +1,13 @@
 
 import nltk
 import collections
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import fuzzyset
+
+nltk.download('stopwords')
 nltk.download('punkt')
+stop = set(stopwords.words('english'))
 
 class SearchIndex:
 	def __init__(self, data, dataKey):
@@ -10,8 +15,14 @@ class SearchIndex:
 		self.ngram_index = {}
 		self.data = data
 		self.dataKey = dataKey
-		for key in self.data.keys():
-			self.add_document(key, self.data[key])
+		self.fuzzyset = fuzzyset.FuzzySet(use_levenshtein=False)
+		documents = []
+		for doc_id in self.data.keys():
+			self.add_document(doc_id, self.data[doc_id])
+			documents.append(self.data[doc_id][dataKey])
+		
+		tfidf = TfidfVectorizer(tokenizer=self.tokenize_document, stop_words='english')
+		self.tfs = tfidf.fit_transform(documents)
 	
 	def add_document(self, doc_id, doc):
 		tokens = self.tokenize_document(doc[self.dataKey])
@@ -19,32 +30,11 @@ class SearchIndex:
 			if not token in self.inverted_index:
 				self.inverted_index[token] = []
 			self.inverted_index[token].append(doc_id)
-
-		ngrams = self.generate_ngrams_from_tokens(tokens, 3)
-		for ngram in ngrams:
-			if not ngram in self.ngram_index:
-				self.ngram_index[ngram] = []
-			self.ngram_index[ngram].append(doc_id)
-	def remove_stopwords_from_tokens(self, tokens):
-		return tokens
-
-	def generate_ngrams_from_tokens(self, tokens, n):
-		return nltk.ngrams(list(" ".join(tokens)), n)
+			self.fuzzyset.add(token)
 
 	def tokenize_document(self, doc):
-		tokens = [x.lower() for x in nltk.word_tokenize(doc)]
-		return self.remove_stopwords_from_tokens(tokens)
-
-	def rank_results(self, results):
-		# TODO: implement TF-IDF based scoring
-		return results
-
-	def get_ngram_results(self, ngrams, seen=None):
-		results = []
-		for ngram in ngrams:
-			if ngram in self.ngram_index:
-				results += self.ngram_index[ngram]
-		return collections.Counter(results)
+		tokens = [x.lower() for x in nltk.word_tokenize(doc) if x not in stop]
+		return tokens
 
 	def get_token_results(self, tokens, seen=None):
 		results = []
@@ -53,18 +43,27 @@ class SearchIndex:
 				results += self.inverted_index[token]
 		return collections.Counter(results)
 
+	def get_fuzzy_results(self, tokens):
+		final_tokens = []
+		for token in tokens:
+			final_tokens += [x[1] for x in self.fuzzyset.get(token)]
+		return self.get_token_results(final_tokens)
+
+	def score_result(self, result, query):
+		return (1.0, result)
+
 	def get_results(self, query, max_hits=100):
 		tokens = self.tokenize_document(query)
-		ngrams = self.generate_ngrams_from_tokens(tokens, 3)
 		results = set()
 		token_results = self.get_token_results(tokens)
-		ngram_results = self.get_ngram_results(ngrams, seen=token_results)
-		print(ngram_results)
-		# all_results = token_results.union(ngram_results)
-		# return [self.data[x] for x in all_results]
-		return []
+		fuzzy_results = self.get_fuzzy_results(tokens)
+		# get tf-idf vector for query
+		a = [self.data[x[0]][self.dataKey] for x in (token_results).most_common()]
+		b = [self.data[x[0]][self.dataKey] for x in (fuzzy_results).most_common()]
+		return sorted([self.score_result(x, query) for x in set(a).union(set(b))], key=lambda x : x[0])
 
-lines = open("/Users/saliksyed/Desktop/CTD_diseases.csv", "r").readlines()
+
+lines = open("/CTD_diseases.csv", "r").readlines()
 diseases = {}
 for idx, line in enumerate(lines[30:]):
 	name = line.split(",")[0]
@@ -76,5 +75,5 @@ for idx, line in enumerate(lines[30:]):
 index = SearchIndex(diseases, 'name')
 
 while True:
-	query = raw_input("Enter search query:")
+	query = input("Enter search query:")
 	print(index.get_results(query))
