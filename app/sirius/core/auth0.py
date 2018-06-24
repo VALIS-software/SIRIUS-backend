0c89b30cf0fc1d0ed199939d17bc7da376615c2b
qyd <saviorbruce@gmail.com>
2018-06-24 15:34:04 -0700

    return string 'Not Logged in' for endpoint /user_profile on dev server

--- app/sirius/core/auth0.py
+++ app/sirius/core/auth0.py
@@ -56,7 +56,7 @@ def requires_auth(f):
 @app.route('/user_profile')
 @requires_auth
 def dashboard():
-    return json.dumps(session['profile'])
+    return json.dumps(session.get('profile', 'Not Logged in'))
 
 @app.route('/logout')
 def logout():
