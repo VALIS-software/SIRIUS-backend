4d9eca11292bdcd5539667ffeab08bf2bbfc6589
qyd <saviorbruce@gmail.com>
2018-07-22 16:36:00 -0700

    fix variant_track bug

--- app/sirius/core/variant_track.py
+++ app/sirius/core/variant_track.py
@@ -7,7 +7,7 @@ from sirius.helpers.loaddata import loaded_genome_contigs
 def get_variants_in_range(contig, start_bp, end_bp, query, verbose=True):
     # lode cached data
     t0 = time.time()
-    query_genome_data, query_start_bps = get_interval_query_results(HashableDict(query))
+    query_genome_data, query_start_bps = get_variant_query_results(HashableDict(query))
     contig_genome_data = query_genome_data[contig]
     contig_start_bps = query_start_bps[contig]
     total_query_count = len(contig_genome_data)
@@ -20,20 +20,19 @@ def get_variants_in_range(contig, start_bp, end_bp, query, verbose=True):
     count_in_range = len(genome_data_in_range)
     t2 = time.time()
     if verbose:
-        print(f"**** get_variants_in_range debug info ***")
-        print(f"-- {total_query_count} variant_results; {t1-t0:.3f} s")
-        print(f"-- Query: {query}")
-        print(f"-- Cache Info {get_variant_query_results.cache_info()}")
-        print(f"-- Found {count_in_range} variant_results in range; {t2-t1:.3f} seconds")
+        debug_message = f"**** get_variants_in_range debug info ***\n"
+        debug_message += f"-- {total_query_count} variant_results; {t1-t0:.3f} s\n"
+        debug_message += f"-- Query: {query}\n"
+        debug_message += f"-- Cache Info {get_variant_query_results.cache_info()}\n"
+        debug_message += f"-- Found {count_in_range} variant_results in range; {t2-t1:.3f} seconds"
+        print(debug_message)
     # form return format
     result = []
     for d in genome_data_in_range:
         result.append({
             'id': d['_id'],
             'start': d['start'],
-            'length': d['length'],
-            'type': d['type'],
-            'name': d['name']
+            'info': d['info']
         })
     return result
 
