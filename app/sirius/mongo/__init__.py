2a88a7cca06fd868588f2b01ae239a5b87507107
qyd <saviorbruce@gmail.com>
2018-07-24 21:49:19 -0700

    hot-patch dev database

--- app/sirius/mongo/__init__.py
+++ app/sirius/mongo/__init__.py
@@ -26,7 +26,9 @@ client = MongoClient(server_address, username=uname, password=pwd, authSource=au
 testdb = client.testdb
 
 # for main database, sirius can only read
-db = client.database1 # we are working with human in the first version.
+# switching between these two databases allow us to 'hot-swap' the database during updates
+db = client.database
+#db = client.database1
 GenomeNodes = db.GenomeNodes
 InfoNodes = db.InfoNodes
 Edges = db.Edges
