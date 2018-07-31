f8600ac2ccff6c8183e590619550bc93e956553c
qyd <saviorbruce@gmail.com>
2018-07-31 12:15:38 -0700

    prevent /call_back url been called by browser history

--- app/sirius/core/auth0.py
+++ app/sirius/core/auth0.py
@@ -24,18 +24,20 @@ auth0 = oauth.register(
 
 @app.route('/callback')
 def callback_handling():
-    # Handles response from token endpoint
-    auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
-    userinfo = resp.json()
-
-    # Store the user information in flask session.
-    session['jwt_payload'] = userinfo
-    session['profile'] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture']
-    }
+    try:
+        # Handles response from token endpoint
+        auth0.authorize_access_token()
+        resp = auth0.get('userinfo')
+        userinfo = resp.json()
+        # Store the user information in flask session.
+        session['jwt_payload'] = userinfo
+        session['profile'] = {
+            'user_id': userinfo['sub'],
+            'name': userinfo['name'],
+            'picture': userinfo['picture']
+        }
+    except:
+        print(f"Warning: /callback url failed.")
     return redirect('/')
 
 @app.route('/login')
