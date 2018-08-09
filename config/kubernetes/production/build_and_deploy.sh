c5b9427b8ef08485ee9efb139277c3f9f965e6c9
qyd <saviorbruce@gmail.com>
2018-08-09 07:58:39 -0700

    simplify production deployment

--- config/kubernetes/production/build_and_deploy.sh
+++ config/kubernetes/production/build_and_deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 SHA=$(echo $(date) | sha1sum | awk '{ print $1 }')
-docker build -t us.gcr.io/valis-194104/sirius:$SHA .
+docker build -t us.gcr.io/valis-194104/sirius:$SHA -f config/kubernetes/production/Dockerfile .
 docker tag us.gcr.io/valis-194104/sirius:$SHA us.gcr.io/valis-194104/sirius:prod
 gcloud docker -- push us.gcr.io/valis-194104/sirius:$SHA
 gcloud docker -- push us.gcr.io/valis-194104/sirius:prod
