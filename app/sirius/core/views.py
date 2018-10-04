e2baabc7f654ece6ff7d9eb05e1ef55c9275875e
qyd <saviorbruce@gmail.com>
2018-10-04 16:15:52 -0700

    improve uploadUrl security

--- app/sirius/core/views.py
+++ app/sirius/core/views.py
@@ -595,6 +595,9 @@ def export_query():
         return abort(404, 'query field not found in jsondata')
     if not upload_url:
         return abort(404, 'uploadUrl field not found in jsondata')
+    # check if the url is safe
+    if not upload_url.startswith('gs://') or any(c in upload_url for c in ' ;&><(){}|`!@#?$%^*'):
+        return abort(404, f'uploadUrl {upload_url} not valid')
     # export as bed
     if file_format == 'bed':
         if query['type'] != QUERY_TYPE_GENOME:
