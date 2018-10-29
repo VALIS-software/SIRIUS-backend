388a5378bde0f8a5a6fe5c34b3bc54dd974b9bb2
qyd <saviorbruce@gmail.com>
2018-10-29 16:58:51 -0700

    use gsutil rsync to update tiledb database, preventing interupted download or decompressing issues

--- app/sirius/tools/deploy_tiledb_to_bucket.py
+++ app/sirius/tools/deploy_tiledb_to_bucket.py
@@ -3,13 +3,18 @@ import subprocess
 
 tiledb_root = os.environ.get('TILEDB_ROOT', None)
 if tiledb_root != None:
-    if os.path.isdir(tiledb_root):
-        os.chdir(tiledb_root)
-        print("Compressing tiledb folders")
-        subprocess.check_call("tar zvcf sirius_fasta_data.tar.gz fasta*/", shell=True)
-        print("Uploading to Cloud Storage Bucket")
-        subprocess.check_call("gsutil cp sirius_fasta_data.tar.gz gs://siriusdata", shell=True)
+    # if os.path.isdir(tiledb_root):
+    #     os.chdir(tiledb_root)
+    #     print("Compressing tiledb folders")
+    #     subprocess.check_call("tar zvcf sirius_fasta_data.tar.gz fasta*/", shell=True)
+    #     print("Uploading to Cloud Storage Bucket")
+    #     subprocess.check_call("gsutil cp sirius_fasta_data.tar.gz gs://siriusdata", shell=True)
+    # else:
+    #     print(f"TileDB root folder {tiledb_root} does not exist, will do nothing here")
+    outstr = subprocess.check_output("gsutil ls gs://siriusdata/tiledb", shell=True)
+    if outstr.startswith("CommandException"):
+        subprocess.run(f"gsutil -m cp -r {tiledb_root} gs://siriusdata/tiledb", shell=True, check=True)
     else:
-        print(f"TileDB root folder {tiledb_root} does not exist, will do nothing here")
+        subprocess.run(f"gsutil -m rsync -r -d gs://siriusdata/tiledb {tiledb_root}", shell=True, check=True)
 else:
     print("TILEDB_ROOT is not set. Will do nothing.")
