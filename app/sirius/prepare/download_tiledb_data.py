29095e19ccc580fc87b6be428647e307e5e21c8b
qyd <saviorbruce@gmail.com>
2018-10-29 19:08:05 -0700

    fallback to the donwloading compressed method since the rsync checksum is too slow

--- app/sirius/prepare/download_tiledb_data.py
+++ app/sirius/prepare/download_tiledb_data.py
@@ -10,32 +10,35 @@ def download_fasta_tiledb():
     dest = os.environ['TILEDB_ROOT']
     if not os.path.isdir(dest):
         os.makedirs(dest)
-    # os.chdir(dest)
-    # filename = 'sirius_fasta_data.tar.gz'
-    # bucket = storage_buckets['siriusdata']
-    # blob = bucket.get_blob(filename)
-    # print(f"Connected to google cloud. File {filename} found.")
+    os.chdir(dest)
+    filename = 'sirius_fasta_data.tar.gz'
+    bucket = storage_buckets['siriusdata']
+    blob = bucket.get_blob(filename)
+    print(f"Connected to google cloud. File {filename} found.")
     # check if local download exists
-    # skip_download = False
-    # if os.path.exists(filename):
-    #     # compare updated time of blob on cloud and local
-    #     local_file_mtime = datetime.fromtimestamp(os.path.getmtime(filename), tz=timezone.utc)
-    #     if local_file_mtime > blob.updated:
-    #         skip_download = True
-    #         print(f"Found local file {filename} newer than cloud blob, skip downloading")
-    #     else:
-    #         print(f"Found local file {filename} older than cloud blob, cleaning files fasta_*")
-    #         subprocess.check_call("rm -r fasta_*", shell=True)
-    # if not skip_download:
-    #     print(f"Downloading {filename} from cloud storage bucket")
-    #     blob.download_to_filename(filename)
-    #     print("File download finished. Extracting")
-    #     subprocess.check_call(f"tar zxf {filename}", shell=True)
-    #     print("File extraction finished")
+    skip_download = False
+    if os.path.exists(filename):
+        # compare updated time of remote and local versions of file
+        local_file_mtime = datetime.fromtimestamp(os.path.getmtime(filename), tz=timezone.utc)
+        if local_file_mtime > blob.updated:
+            skip_download = True
+            print(f"Found local file {filename} newer than cloud blob, skip downloading")
+        else:
+            print(f"Found local file {filename} older than cloud blob, cleaning files fasta_*")
+            subprocess.check_call("rm -r fasta_*", shell=True)
+    if not skip_download:
+        print(f"Downloading {filename} from cloud storage bucket")
+        blob.download_to_filename(filename)
+        print("File download finished. Extracting")
+        subprocess.check_call(f"tar zxf {filename}", shell=True)
+        print("File extraction finished")
 
-    # use rsync command to download and sync tiledb folder
-    print(f"Running rsync to download tiledb folder")
-    subprocess.run(f"gsutil -m rsync -r {dest} gs://siriusdata/tiledb > rsync.log", shell=True, check=True)
+
+    ## 2nd method: use rsync command to download and sync tiledb folder
+    ## This method is more robust, which can avoid interupted downloading and unzipping
+    ## However the rsync checksums is too slow in container, so we fallback to the first method
+    # print(f"Running rsync to download tiledb folder")
+    # subprocess.run(f"gsutil -m rsync -r {dest} gs://siriusdata/tiledb > rsync.log", shell=True, check=True)
 
 if __name__ == "__main__":
      download_fasta_tiledb()
--- app/sirius/tools/deploy_tiledb_to_bucket.py
+++ app/sirius/tools/deploy_tiledb_to_bucket.py
@@ -4,19 +4,22 @@ import subprocess
 def upload_tiledb_to_cloud():
     tiledb_root = os.environ.get('TILEDB_ROOT', None)
     if tiledb_root != None:
-        # if os.path.isdir(tiledb_root):
-        #     os.chdir(tiledb_root)
-        #     print("Compressing tiledb folders")
-        #     subprocess.check_call("tar zvcf sirius_fasta_data.tar.gz fasta*/", shell=True)
-        #     print("Uploading to Cloud Storage Bucket")
-        #     subprocess.check_call("gsutil cp sirius_fasta_data.tar.gz gs://siriusdata", shell=True)
-        # else:
-        #     print(f"TileDB root folder {tiledb_root} does not exist, will do nothing here")
-        outstr = subprocess.check_output("gsutil ls gs://siriusdata/tiledb", shell=True)
-        if outstr.startswith("CommandException"):
-            subprocess.run(f"gsutil -m cp -r {tiledb_root} gs://siriusdata/tiledb", shell=True, check=True)
+        # 1st method: compress the fasta* files in tiledb folder then upload
+        if os.path.isdir(tiledb_root):
+            os.chdir(tiledb_root)
+            print("Compressing tiledb folders")
+            subprocess.check_call("tar zvcf sirius_fasta_data.tar.gz fasta*/", shell=True)
+            print("Uploading to Cloud Storage Bucket")
+            subprocess.check_call("gsutil cp sirius_fasta_data.tar.gz gs://siriusdata", shell=True)
         else:
-            subprocess.run(f"gsutil -m rsync -r -d gs://siriusdata/tiledb {tiledb_root}", shell=True, check=True)
+            print(f"TileDB root folder {tiledb_root} does not exist, will do nothing here")
+
+        # 2nd method: using rsync to sync the tiledb folder to cloud bucket
+        # outstr = subprocess.check_output("gsutil ls gs://siriusdata/tiledb", shell=True)
+        # if outstr.startswith("CommandException"):
+        #     subprocess.run(f"gsutil -m cp -r {tiledb_root} gs://siriusdata/tiledb", shell=True, check=True)
+        # else:
+        #     subprocess.run(f"gsutil -m rsync -r -d gs://siriusdata/tiledb {tiledb_root}", shell=True, check=True)
     else:
         print("TILEDB_ROOT is not set. Will do nothing.")
 
