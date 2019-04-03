64817e783085414b80097d74baef22b3e7eeb5ee
Salik Syed <ssyed@cs.stanford.edu>
2019-04-03 12:02:51 -0700

    gut db code

--- app/sirius/mongo/__init__.py
+++ app/sirius/mongo/__init__.py
@@ -1,26 +1,11 @@
 import os
 from pymongo import MongoClient
 
-# allow user specify authorization
-try:
-    uname, pwd = os.environ["MONGO_UNAME"], os.environ["MONGO_PWD"]
-    authenticationDatabase = 'admin'
-except:
-    uname, pwd = 'sirius', 'valis'
-    authenticationDatabase = 'testdb'
 
-if 'KUBERNETES_SERVICE_HOST' in os.environ:
-    # we currently have one replica, more can be added here
-    server_address = 'mongo-0.mongo-service'
-elif 'MONGO_PORT' in os.environ:
-    # This is used when we have container linkage like --link some-mongo:mongo when debugging inside docker containers
-    server_address = 'mongo'
-elif 'MONGO_HOST' in os.environ:
-    # For specifying mongo host manually
-    server_address = os.environ['MONGO_HOST']
-else:
-    # use kubectl port-forward mongo-0 27017:27017 to forward port if dev locally
-    server_address = 'localhost'
+uname, pwd = 'sirius', 'valis'
+authenticationDatabase = 'testdb'
+
+server_address = 'localhost'
 
 
 client = MongoClient(server_address, username=uname, password=pwd, authSource=authenticationDatabase, connect=False)
@@ -36,9 +21,3 @@ GenomeNodes = db.GenomeNodes
 InfoNodes = db.InfoNodes
 Edges = db.Edges
 
-# The user database for storing user-uploaded private data
-userdb = client.userdb
-UserInfo = userdb.UserInfo
-
-# The analysis database that works with the analysis engine
-analysisdb = client.analysisdb
